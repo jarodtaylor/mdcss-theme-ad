fa30f339681ac3e5c5a71dac2e8afe894599017a
jarodtaylor <jarodrtaylor@gmail.com>
2017-04-18 15:45:54 -0400

    initial commit POC

---
+++ README.md
@@ -0,0 +1,32 @@
+# aetna-digital
+
+<img align="right" width="96" height="96" src="https://i.imgur.com/3rqeZXi.png" title="logo of mdcss">
+
+[![NPM Version][npm-img]][npm] [![Build Status][ci-img]][ci]
+
+aetna-digital is a theme for [mdcss].
+
+## Usage
+
+Add [mdcss] and [aetna-digital] to your build tool:
+
+```bash
+npm install mdcss --save-dev
+npm install mdcss-theme-aetna-digital --save-dev
+```
+
+Whenever [mdcss] is used, reference this theme.
+
+```js
+require('mdcss')({
+	theme: require('mdcss-theme-aetna-digital')({ /* options */ })
+})
+```
+
+[ci]:      https://travis-ci.org/jarodtaylor/mdcss-theme-aetna-digital
+[ci-img]:  https://img.shields.io/travis/jarodtaylor/mdcss-theme-aetna-digital.svg
+[npm]:     https://www.npmjs.com/package/mdcss-theme-aetna-digital
+[npm-img]: https://img.shields.io/npm/v/mdcss-theme-aetna-digital.svg
+[mdcss]:   https://github.com/jonathantneal/mdcss
+
+[aetna-digital]: https://github.com/jarodtaylor/mdcss-theme-aetna-digital
---
+++ index.js
@@ -0,0 +1,61 @@
+var ejs  = require('ejs');
+var fs   = require('fs');
+var path = require('path');
+var ext = require('object-assign');
+
+module.exports = function (themeopts) {
+	// set theme options object
+		themeopts = Object(themeopts);
+
+		// set theme title
+		themeopts.title = themeopts.title || 'Style Guide';
+
+		// set theme css
+		themeopts.css = themeopts.css || [];
+
+		// set theme js
+		themeopts.js = themeopts.js || [];
+
+	  // set navigation links
+		themeopts.nav = themeopts.nav || [];
+
+		// set example conf
+		themeopts.examples = ext({
+			base:    '',
+			target:  '_self',
+			css:     ['style.css'],
+			js:      [],
+			bodyjs:  [],
+			htmlcss: 'background:none;border:0;clip:auto;display:block;height:auto;margin:0;padding:0;position:static;width:auto',
+			bodycss: 'background:none;border:0;clip:auto;display:block;height:auto;margin:0;padding:16px;position:static;width:auto'
+		}, themeopts.examples);
+
+	// return theme
+	return function (docs) {
+
+		// set assets directory and template
+		docs.assets   = path.join(__dirname, 'assets');
+		docs.template = path.join(__dirname, 'template.ejs');
+
+		// set theme options
+		docs.themeopts = themeopts;
+
+		// return promise
+		return new Promise(function (resolve, reject) {
+			// read template
+			fs.readFile(docs.template, 'utf8', function (error, contents) {
+				// throw if template could not be read
+				if (error) reject(error);
+				else {
+					// set compiled template
+					docs.template = ejs.compile(contents)(docs);
+
+					// resolve docs
+					resolve(docs);
+				}
+			});
+		});
+	};
+};
+
+module.exports.type = 'mdcss-theme';
